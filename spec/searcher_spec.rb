72a364fe3cdd8afbe42a16a5f161b7671331d2fd
hitode909 <hitode909@gmail.com>
2010-12-14 19:40:25 +0900

    searcher

--- spec/searcher_spec.rb
+++ spec/searcher_spec.rb
@@ -24,25 +24,4 @@ describe Kindai::Searcher do
     count.should == @searcher.length
   end
 
-
-  # it 'has total page' do
-  #   @book.total_page.should == 31
-  # end
-
-  # it 'has total sprea' do
-  #   @book.total_spread.should == 20
-  # end
-
-  # it 'has author' do
-  #   @book.author.should == '正義熱血社'
-  # end
-
-  # it 'has spreads' do
-  #   @book.spreads.should have_exactly(@book.total_spread).spreads
-  # end
-
-  # it 'has base_uri' do
-  #   @book.base_uri.should == "http://kindai.da.ndl.go.jp/scrpt/ndlimageviewer-rgc.aspx?pid=info%3Andljp%2Fpid%2F922693&jp=42016454&vol=10010&koma=1&vs=10000,10000,0,0,0,0,0,0"
-  # end
-
 end
