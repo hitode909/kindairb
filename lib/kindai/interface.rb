945ea63c95baf0f3b607f12fb2f685a597f20ddd
hitode909 <hitode909@gmail.com>
2010-12-14 19:43:36 +0900

    interface

--- lib/kindai/interface.rb
+++ lib/kindai/interface.rb
@@ -2,28 +2,28 @@
 module Kindai::Interface
   def self.download_url(url, config = { })
     book = Kindai::Book.new_from_permalink(url)
-    downloader = Kindai::Downloader.new_from_book(book)
+    downloader = Kindai::BookDownloader.new_from_book(book)
     downloader.output_directory = config[:output_directory] if config[:output_directory]
     downloader.retry_count = config[:retry_count] if config[:retry_count]
-    Kindai::Util.logger.info "download #{book.title}(#{book.page} pages) to #{downloader.full_directory_path}"
+    Kindai::Util.logger.info "download #{book.title}(#{book.total_page} pages) to #{downloader.book_path}"
     downloader.download
 
-    publisher = Kindai::Publisher.new_from_book(book)
-    publisher.use_divide if config[:use_divide]
-    publisher.use_pdf if config[:use_pdf]
-    publisher.use_zip if config[:use_zip]
-    publisher.test_mode if config[:test_mode]
-    publisher.resize_option = config[:resize_option] if config[:resize_option]
-    publisher.set_trimming(
-      :x => config[:trimming_x] || 0,
-      :y => config[:trimming_y] || 0,
-      :w => config[:trimming_w] || 5000,
-      :h => config[:trimming_h] || 5000,
-      ) if config[:trimming_x] || config[:trimming_y] || config[:trimming_w] || config[:trimming_h]
-    publisher.publish
+    # publisher = Kindai::Publisher.new_from_book(book)
+    # publisher.use_divide if config[:use_divide]
+    # publisher.use_pdf if config[:use_pdf]
+    # publisher.use_zip if config[:use_zip]
+    # publisher.test_mode if config[:test_mode]
+    # publisher.resize_option = config[:resize_option] if config[:resize_option]
+    # publisher.set_trimming(
+    #   :x => config[:trimming_x] || 0,
+    #   :y => config[:trimming_y] || 0,
+    #   :w => config[:trimming_w] || 5000,
+    #   :h => config[:trimming_h] || 5000,
+    #   ) if config[:trimming_x] || config[:trimming_y] || config[:trimming_w] || config[:trimming_h]
+    # publisher.publish
 
-    publisher.publish_trimmed_pdf
-    publisher.publish_kindle
+    # publisher.publish_trimmed_pdf
+    # publisher.publish_kindle
   end
 
   def self.download_keyword(keyword, config = { })
